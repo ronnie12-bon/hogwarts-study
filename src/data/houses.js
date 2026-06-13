707cd6680f28305a99a88e369ce55093abf9a307
ronnie12-bon <sharonlee080812@gmail.com>
2026-06-13 21:45:23 +0800

    Create houses.js

---
+++ src/data/houses.js
@@ -0,0 +1,22 @@
+export const houses = [
+  {
+    name: "Gryffindor",
+    logo: "/images/houses/gryffindor.png",
+    color: "#740001"
+  },
+  {
+    name: "Slytherin",
+    logo: "/images/houses/slytherin.png",
+    color: "#1A472A"
+  },
+  {
+    name: "Ravenclaw",
+    logo: "/images/houses/ravenclaw.png",
+    color: "#0E1A40"
+  },
+  {
+    name: "Hufflepuff",
+    logo: "/images/houses/hufflepuff.png",
+    color: "#ECB939"
+  }
+];
